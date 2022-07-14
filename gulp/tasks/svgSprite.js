2f7481ec1e6f9f4b7a69967fb7718a28c2e47894
Transparency010101 <ossys1010110@gmail.com>
2022-07-15 02:36:24 +0300

    сделал alchohole(знаю я не граматей). нужно адаптировать

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -14,6 +14,7 @@ export const svgSprive = () => {
             mode: {
                 stack: {
                     sprite: '../icons/icons.svg',
+                    example: true,
                 }
             },
         }))
