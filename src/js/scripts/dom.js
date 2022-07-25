1c1d44ef05832eeb55cc19aff19eff07c5619efa
Transparency010101 <ossys1010110@gmail.com>
2022-07-25 19:14:13 +0300

    щас буду делать spoiler

--- src/js/scripts/dom.js
+++ src/js/scripts/dom.js
@@ -1,1 +1,18 @@
 // * тут небольшие DOM/BOM скрипты
+
+// * Аккордион для stay
+const stayTitles = document.querySelectorAll(".stay-list__title");
+
+stayTitles.forEach(title => {
+   title.addEventListener("click", function () {
+
+      stayTitles.forEach(titleRemove => {
+         titleRemove.classList.remove("active");
+         titleRemove.nextElementSibling.classList.remove("active");
+      });
+
+      title.classList.add("active");
+      title.nextElementSibling.classList.add("active");
+   });
+});
+
