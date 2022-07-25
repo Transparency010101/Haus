16805a71d5444b32439ed2f788e6f5f28aeed1cd
Transparency010101 <ossys1010110@gmail.com>
2022-07-26 02:16:06 +0300

    сделал так как почти хотел

--- src/js/scripts/dom.js
+++ src/js/scripts/dom.js
@@ -2,11 +2,13 @@
 
 // * Споилер для stay
 const stayTitles = document.querySelectorAll(".stay-list__title");
+const stayList = document.querySelectorAll('.stay-list__list');
 
 stayTitles.forEach(title => {
    title.addEventListener("click", function (e) {
-      this.classList.toggle("active");
-      this.nextElementSibling.classList.toggle("active");
+      title.nextElementSibling.style.maxHeight = 0;
+      title.classList.toggle("active");
+      title.nextElementSibling.classList.toggle('active');
+      title.nextElementSibling.style.maxHeight = title.nextElementSibling.scrollHeight + 'px';
    });
 });
-
